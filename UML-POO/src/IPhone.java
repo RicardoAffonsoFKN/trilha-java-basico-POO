2f772f1bc085159c1d09de319fd180a1ebddbfd4
RicardoAffonsoFKN <167235069+RicardoAffonsoFKN@users.noreply.github.com>
2024-09-02 20:59:02 -0300

    Entrega desafio UML e POO

---
+++ UML-POO/src/IPhone.java
@@ -0,0 +1,13 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorNaInternet{
+    public void tocar(){}
+    public void pausar(){}
+    public void selecionarMusica(String musica){}
+
+    public void exibirPagina(String url){}
+    public void adicionarNovaAba(){}
+    public void atualizarPagina(){}
+
+    public void ligar(String numero){}
+    public void atender(){}
+    public void iniciarCorreioVoz(){}
+}
